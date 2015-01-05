0e14d60cb2c5977bd0ec478af1795c7ad5d2576b
Euphe <kururugisuzakueuphe@ya.ru>
2015-01-05 17:07:37 +0400

    Track only world data

--- save.bat
+++ save.bat
@@ -2,7 +2,7 @@ set uname= MuffinZerg
 set cataPath = C:\games\Cataclysm\save\SharedCataclysm
 
 cd %cataPath%
-git pull
-git add *
+git add *.map
+git add *.0.0
 git commit -m "%uname%"
 git push -u origin master
