87cf1c880eaa0f54606df271e42e399fa283cc22
Euphe <kururugisuzakueuphe@ya.ru>
2015-01-06 19:29:42 +0400

    save changes

--- save.bat
+++ save.bat
@@ -2,8 +2,8 @@ set commitmsg= save changes
 set cataPath = C:\games\Cataclysm\save\SharedCataclysm
 
 cd %cataPath%
-git add *.map
-git add *.0.0
+git add maps/*
+git add o.*
 git commit -m "%commitmsg%"
 git push -u origin master
 
